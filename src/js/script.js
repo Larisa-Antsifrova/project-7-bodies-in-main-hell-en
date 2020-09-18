22a0df826891c744651de60d507728eea029c4ea
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-09-18 15:56:25 +0300

    Edited scripts.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-  $('.slider-container').slick({});
+  $('.slider-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    autoplay: true,
+  });
 });
